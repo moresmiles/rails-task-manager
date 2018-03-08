65ed5d4e195a4336ceeeb0e58389aacb42e34369
github username <github email address>
2018-03-08 11:53:25 +0200

    list create action, and list_params

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,6 +4,23 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path(@list)
+    else
+      flash[:alert] = "Unable to create list, please try again"
+      redirect_to new_list_path
+    end
+  end
+
   def show
+    @lists = List.all
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
