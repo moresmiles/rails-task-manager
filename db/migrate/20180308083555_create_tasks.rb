d875afad907e4457958b2b016aa06508b8a7c12f
github username <github email address>
2018-03-08 10:46:50 +0200

    migrate tasks table

--- db/migrate/20180308083555_create_tasks.rb
+++ db/migrate/20180308083555_create_tasks.rb
@@ -1,6 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
+      t.string :name
+      t.integer :user_id
+      t.integer :list_id
+      t.integer :status, :default => 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308083214) do
+ActiveRecord::Schema.define(version: 20180308083555) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.integer "list_id"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
