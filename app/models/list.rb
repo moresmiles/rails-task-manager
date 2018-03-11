7b6eae037d04dc925a13b6dca84469d5c1c9c899
github username <github email address>
2018-03-11 23:44:18 +0200

    custom attribute writer #tasks_attributes=

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,13 @@
 class List < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, inverse_of: :list
   has_many :users, through: :tasks
-
   validates :name, presence: true
+
+ def tasks_attributes=(tasks_attributes)
+    tasks_attributes.values.each do |task_attribute|
+      task = Task.find_or_create_by(name: task_attribute[:name])
+      self.tasks << task
+    end
+  end
 
 end
