354982636f2c3bdb5fc5eb29a0454932bd0fbeef
github username <github email address>
2018-03-13 09:17:55 +0200

    list belongs_to group

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,12 +1,17 @@
 class List < ApplicationRecord
   has_many :tasks, inverse_of: :list
   has_many :users, through: :tasks
+  belongs_to :group
   validates :name, presence: true
 
  def tasks_attributes=(tasks_attributes)
     tasks_attributes.values.each do |task_attribute|
-      task = Task.find_or_create_by(name: task_attribute[:name])
-      self.tasks << task
+      if task_attribute[:name].present?
+        task = Task.find_or_create_by(name: task_attribute[:name])
+        if !self.tasks.include?(task)
+          self.tasks << task
+        end
+      end
     end
   end
 
