2a1a3f4e335a9e149f4912aedc9340acec84eda8
github username <github email address>
2018-03-13 09:18:49 +0200

    group associations, and user_attributes custom writer

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,18 @@
 class Group < ApplicationRecord
+  has_many :group_users, inverse_of: :group
+  has_many :users, through: :group_users
+  has_many :lists
+
+
+  def users_attributes=(users_attributes)
+     users_attributes.values.each do |user_attribute|
+       if user_attribute[:email].present?
+         user = User.find_by(email: user_attribute[:email])
+         if user && !self.users.include?(user)
+           self.users << user
+         end
+       end
+     end
+   end
+
 end
