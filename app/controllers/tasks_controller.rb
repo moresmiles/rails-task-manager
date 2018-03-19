6675410d745cd40c91a7cbe47ac12a21150d146e
github username <github email address>
2018-03-19 09:38:52 +0200

    edit, update, destroy actions for task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,24 @@
 class TasksController < ApplicationController
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to group_list_path(@task.list.group, @task.list)
+  end
+
+  def destroy
+    @list = List.find_by(params[:list_id])
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to group_list_path(@list.group, @list)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:status, :user_id)
+  end
 end
