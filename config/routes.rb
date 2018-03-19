982c213ee4be67119f2cbd9dc99ec9633db86766
github username <github email address>
2018-03-19 09:44:06 +0200

    updated routes, using only routes that are needed

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :groups do
-    resources :lists
+  resources :users, only: [:show, :destroy]
+
+  resources :groups, only: [:new, :create :edit, :update, :destroy] do
+    resources :lists, only: [:new, :create, :edit, :update, :show, :destroy]
   end
-    resources :tasks
+    resources :tasks, only: [:edit, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
