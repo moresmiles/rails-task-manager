15ecabb92f39ea12bc728dc7f02cc33b08cd22ba
github username <github email address>
2018-03-19 14:41:21 +0200

    class level scope method self.incomplete & incomplete tasks table

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,11 +3,15 @@ class Task < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :list, optional: true
 
-  def completed?
+  def task_status
     if self.status == 0
       "Incomplete"
     elsif self.status == 1
       "Done!"
     end
+  end
+
+  def self.incomplete
+    where("status = 0")
   end
 end
