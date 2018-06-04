6736bda6f91cec80573b41544e1e7a08d541e8f6
github username <github email address>
2018-06-04 09:41:29 +0300

    enabled submit button to work w/out page refresh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_view.automatically_disable_submit_tag = false
 
   # Code is not reloaded between requests.
   config.cache_classes = true
